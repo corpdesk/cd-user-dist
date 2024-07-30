53d11d28dfb7c82a670efffeac464f23f469f8ec
“georemo” <george.oremo@gmail.com>
2024-07-30 10:40:56 +0300

    -

---
+++ 8596.862a8d6310f4dea0.js
@@ -0,0 +1,1 @@
+(self.webpackChunkcd_user=self.webpackChunkcd_user||[]).push([[8596],{8596:(r,t,e)=>{e.r(t),e.d(t,{TranslateHttpLoader:()=>i});class i{http;prefix;suffix;constructor(s,a="/assets/i18n/",h=".json"){this.http=s,this.prefix=a,this.suffix=h}getTranslation(s){return this.http.get(`${this.prefix}${s}${this.suffix}`)}}}}]);
